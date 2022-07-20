8cac88f9560d5c2dd05c3b3c90b8626a3b02ea73
ASISH DUTTA <asishdutta@ASISHs-iMac.local>
2022-07-20 18:01:41 +1000

    Few changes in all files

--- script/dice.js
+++ script/dice.js
@@ -1,20 +1,30 @@
-
+var randNo1 , randNo2;
+var randImage1;
+var randImage2;
+var total;
 function rollCount()
     {
-        var randNo1 = Math.floor(Math.random()*6)+1;
-        var randNo2 = Math.floor(Math.random()*6)+1;
-        var randImage1 = "images/dice" + randNo1 + ".png";
-        var randImage2 = "images/dice" + randNo2 + ".png";
-        document.querySelectorAll("img")[0].setAttribute("src", randImage1);
-        document.querySelectorAll("img")[1].setAttribute("src", randImage2);
-        var total = randNo1 + randNo2;
+        
+        
+        randNo1 = Math.floor(Math.random()*6)+1;
+        randNo2 = Math.floor(Math.random()*6)+1;
+        randImage1 = "images/dice" + randNo1 + ".png";
+        randImage2 = "images/dice" + randNo2 + ".png";
+
+        // document.querySelectorAll("img")[0].setAttribute("src", randImage1);           
+        // document.querySelectorAll("img")[1].setAttribute("src", randImage2); 
+        document.getElementById("img1").src=randImage1;  
+        document.getElementById("img2").src=randImage2;  
+
+        total = randNo1 + randNo2;
         
         document.getElementById("rollCount").style.color = "red";
         document.getElementById("rollCount").innerHTML = total;
         document.getElementById("rollCount").style.fontSize = "xxx-large";
     }
 
-// if (randNo1 > randNo2)
+
+    // if (randNo1 > randNo2)
 // {
 //   document.querySelector("h1").innerHTML = "Player 1 Won";
 // }
