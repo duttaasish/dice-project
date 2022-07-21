54598a27412b5f02bb09df5f46eeae97ec2271d6
ASISH DUTTA <asishdutta@ASISHs-iMac.local>
2022-07-21 21:34:14 +1000

    Few changes done all files

--- script/dice.js
+++ script/dice.js
@@ -1,38 +1,45 @@
+
 var randNo1 , randNo2;
 var randImage1;
 var randImage2;
 var total;
-function rollCount()
-    {
-        
-        
-        randNo1 = Math.floor(Math.random()*6)+1;
-        randNo2 = Math.floor(Math.random()*6)+1;
-        randImage1 = "images/dice" + randNo1 + ".png";
-        randImage2 = "images/dice" + randNo2 + ".png";
+var j=0;
 
-        // document.querySelectorAll("img")[0].setAttribute("src", randImage1);           
-        // document.querySelectorAll("img")[1].setAttribute("src", randImage2); 
-        document.getElementById("img1").src=randImage1;  
-        document.getElementById("img2").src=randImage2;  
+function rollCountx(callback, interval, repeatTimes)
+    {   
+      let repeated = 0;
+      const intervalTask = setInterval(doTask , interval);
 
-        total = randNo1 + randNo2;
-        
-        document.getElementById("rollCount").style.color = "red";
-        document.getElementById("rollCount").innerHTML = total;
-        document.getElementById("rollCount").style.fontSize = "xxx-large";
+      function doTask()
+      {
+          if (repeated <repeatTimes)
+          {
+              callback();
+              repeated +=1;
+          }
+          else
+          {
+            clearInterval(intervalTask);
+            total = randNo1 + randNo2;  
+            document.getElementById("rollCount").style.color = "black";
+            document.getElementById("rollCount").innerHTML = total;
+            document.getElementById("rollCount").style.fontSize = "xxx-large";   
+          }
+      }
+    
     }
 
-
-    // if (randNo1 > randNo2)
-// {
-//   document.querySelector("h1").innerHTML = "Player 1 Won";
-// }
-// else if (randNo2 > randNo1)
-// {
-//   document.querySelector("h1").innerHTML = "Player 2 Won";
-// }
-// else
-// {
-//   document.querySelector("h1").innerHTML = "Draw...";
-// }
+function rollCount()
+    {            
+        randNo1 = Math.floor(Math.random()*6)+1;
+        randNo2 = Math.floor(Math.random()*6)+1;
+        randImage1 = "images/dice" + randNo1 + ".jpg";
+        randImage2 = "images/dice" + randNo2 + ".jpg";
+        document.getElementById("img1").src=randImage1;        
+        document.getElementById("img2").src=randImage2;  
+    }     
+        
+    function roll()
+    {
+        rollCountx(rollCount, 100, 10);        
+    }
